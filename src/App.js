25f545a54c5ac4f8be6d2fdff01a7c57bf02304c
Unnamed999999999 <Unnamed999999999@Gmail.com>
2018-04-24 18:30:16 +0300

    fix #2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,8 @@ import './App.css';
 
 import Homepage from './components/pageBuilder/homepage';
 
-<<<<<<< HEAD
 
 class App extends React.Component {
-=======
-  //hi
-
-  getItems(){
-    fetch('http://localhost:63234/api/Message/GetMessage/')
-    .then(results => results.json())
-    .then(results => this.setState({'items': results}));
-  }
->>>>>>> b649648fc42bf5d6b8e55ffc83802ba04265da80
   render() {
     return (
       <div className="app">
