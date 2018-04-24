3be9da2680e2ae7940a835e8b952fe3bcc70f6e0
Unnamed999999999 <Unnamed999999999@Gmail.com>
2018-04-24 20:05:32 +0300

    footer

--- src/components/layoutElements/footer.js
+++ src/components/layoutElements/footer.js
@@ -3,10 +3,17 @@ import * as React from 'react';
 class Footer extends React.Component{
     render(){
         return(
-            <footer>
-            <p>&copy;{(new Date()).getFullYear()} - Softserve Academy</p>
-            <p>Created by Lv-310.Net</p>
-        </footer>
+            <footer className="footer bg-dark fixed-bottom">
+                <div className="copyright container">
+                    <div className="row spacer-25-vertical"/>
+                        <div className="row justify-content-md-center">
+                            <div className="col-md-auto text-light align-self-center">
+                            &copy;{(new Date()).getFullYear()} - Softserve IT Academy. Created by Lv-310.Net  
+                            </div>
+                        </div>
+                    <div className="row spacer-25-vertical"/>     
+                </div>
+            </footer>
         );
     }
 }
--- src/components/pageBuilder/homepage.js
+++ src/components/pageBuilder/homepage.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import NavBar from '../layoutElements/navbar/navbar';
+import Footer from '../layoutElements/footer';
 
 class Homepage extends React.Component{
     render() {
         return(
+            <div>
                 <NavBar />
+                <Footer />
+            </div>
         );
     }
 }
